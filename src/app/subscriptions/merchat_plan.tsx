ef22ccc65c539be3ca485d92af2c962e6f8b2442
DAVIS ISIBOR <davisisibor@gmail.com>
2023-11-13 02:07:58 +0100

    Update merchat_plan.tsx

--- src/app/subscriptions/merchat_plan.tsx
+++ src/app/subscriptions/merchat_plan.tsx
@@ -7,19 +7,21 @@ import Image from "next/image";
 const Merchant_plan = () => {
   return (
     <>
-      <div className="w-[29vw] sm:w-[90vw] sm:h-[120vw]   h-[50vw] pt-[2vw] pb-[2vw] px-[1.2vw] flex flex-col justify-center items-center  subscription_merchant_background_color rounded-[1.2vw] border-l-[0.5vw] gap-[2vw]  border-[#CCFF00]">
+      <div className="w-[29vw] sm:w-[80vw] sm:h-[130vw] sm:border-l-[1.6vw]  sm:gap-[4vw] sm:px-[6vw] sm:rounded-[4vw]   h-[50vw] pt-[2vw] pb-[2vw] px-[1.2vw] flex flex-col justify-center items-center  subscription_merchant_background_color rounded-[1.2vw] border-l-[0.5vw] gap-[2vw]  border-[#CCFF00]">
         {/* first row div */}
         <div className="w-full flex justify-between items-center">
           <Image
             src={mer_1}
-            className="w-[3vw] sm:w-[10vw] h-fit"
+            className="w-[3vw]  sm:w-[15vw] h-fit"
             alt="most popular image placeholder"
           />
         </div>
         {/* second row div */}
         <div className="w-full flex  flex-col gap-[0.2vw]  ">
-          <h2 className="text-[2.1vw] text-black neuem">Merchant Tier</h2>
-          <h3 className="text-[#000000] neuer text-opacity-[60%] text-[0.9vw]">
+          <h2 className="text-[2.1vw] text-black neuem sm:text-[6vw]">
+            Merchant Tier
+          </h2>
+          <h3 className="text-[#000000] sm:text-[2.4vw] neuer text-opacity-[60%] text-[0.9vw]">
             The merchant tier2 is a second wave of merchants that gives you
             commercial license to 3D prints and sell station forge models in
             physical forms only as long as subscribed this tier is only limited
@@ -29,16 +31,16 @@ const Merchant_plan = () => {
 
         {/* thired div  */}
         <div className="w-full">
-          <h3 className="text-black text-[2.3vw] neuem ">
+          <h3 className="text-black text-[2.3vw] neuem sm:text-[4vw] ">
             $100{" "}
-            <span className="text-opacity-[60%] text-black text-[1.2vw]">
+            <span className="text-opacity-[60%] text-black text-[1.2vw] sm:text-[3vw]">
               /Month
             </span>
           </h3>
         </div>
 
         {/* fourthe div */}
-        <p className="text-[1vw]">
+        <p className="text-[1vw] sm:text-[2.6vw]">
           Permission to print and distribute physical models (not the STL.
           files) through your website/marketplaces. Use the original pictures
           only from the page with the Station Forge logo. Re-rendering photos is
@@ -51,7 +53,7 @@ const Merchant_plan = () => {
         </p>
 
         {/* fivth div  also known as button */}
-        <button className="w-full h-[4.3vw] text-[1.6vw] neuem rounded-[3.7vw] transition duration-[0.2s] hover:bg-opacity-[80%] bg-opacity-[100%] bg-[#000000] text-white ">
+        <button className="w-full h-[4.3vw] text-[1.6vw] sm:rounded-[5vw]  sm:text-[4vw] sm:h-[10vw] neuem rounded-[3.7vw] transition duration-[0.2s] hover:bg-opacity-[80%] bg-opacity-[100%] bg-[#000000] text-white ">
           Join
         </button>
       </div>{" "}
