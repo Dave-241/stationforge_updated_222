3b83f0412837e92e89454f4b977fcbcc9b3f81fa
DAVIS ISIBOR <106489608+Dave-241@users.noreply.github.com>
2023-11-19 11:05:21 +0100

    Delete src/app/types directory

--- src/app/types/modalPropTypes.ts
+++
@@ -1,6 +0,0 @@
-export type ModalPropsTypes = {
-  name?: string;
-  handleModalPopUp: Function;
-  digitalSalesData?: any[];
-  userId?: string
-};
--- src/app/types/monthlyforgeallocation.ts
+++
@@ -1,4 +0,0 @@
-export type MonthlyForgeAllocationsTypes = {
-  handleModalPopUp: Function;
-  digitalSalesData?: any[];
-};
