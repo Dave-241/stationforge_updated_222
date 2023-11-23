29ad455693c8f1370555d0a6217c4ef26010ba39
DAVIS ISIBOR <davisisibor@gmail.com>
2023-11-23 08:13:51 +0100

    Update page.tsx

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -10,6 +10,14 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "@/app/utils/fire_base_config";
 import { useRouter } from "next/navigation";
 import Dashboard_hero_section from "./dashboard_hero";
+import {
+  Timestamp,
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default function Home() {
   const {
@@ -20,12 +28,23 @@ export default function Home() {
     setfrom,
   }: any = useProfile_Context();
   const [showdash, setshowdash] = useState(false);
+  const [downloadsData, setDownloadsData] = useState([]);
+  const [currentWeekDownloads, setCurrentWeekDownloads] = useState(0);
+  const [previousMonthDownloads, setPreviousMonthDownloads] = useState(0);
+  const [currentMonthDownloads, setCurrentMonthDownloads] = useState(0);
+
+  const [previousWeekDownloads, setPreviousWeekDownloads] = useState(0);
+  const [percentageChange, setPercentageChange] = useState(0);
   useEffect(() => {
     // setpage_loader(false);
     setfrom("");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Firestore
+  const db = getFirestore(app);
   const auth: any = getAuth();
   const route = useRouter();
   // Use useEffect to check if the user is already authenticated
@@ -54,6 +73,103 @@ export default function Home() {
     return () => unsubscribe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    console.log("useEffect started");
+
+    const fetchData = async () => {
+      console.log("Fetching data...");
+
+      try {
+        // Initialize Firestore
+        const db = getFirestore();
+
+        // Define the 'library' collection
+        const libraryCollection = collection(db, "libray");
+
+        // Define the current month's start and end timestamps
+        const currentDate = new Date();
+        const currentMonthStart = Timestamp.fromDate(
+          new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+        );
+        const currentMonthEnd = Timestamp.fromDate(
+          new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth() + 1,
+            0,
+            23,
+            59,
+            59,
+          ),
+        );
+
+        // Define the previous month's start and end timestamps
+        const previousMonthStart = Timestamp.fromDate(
+          new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1),
+        );
+        const previousMonthEnd = Timestamp.fromDate(
+          new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            0,
+            23,
+            59,
+            59,
+          ),
+        );
+
+        // Define the queries for the current and previous months
+        const currentMonthQuery = query(
+          libraryCollection,
+          where("createdAt", ">=", currentMonthStart),
+          where("createdAt", "<=", currentMonthEnd),
+          where("downloaded", "==", true),
+        );
+        const previousMonthQuery = query(
+          libraryCollection,
+          where("createdAt", ">=", previousMonthStart),
+          where("createdAt", "<=", previousMonthEnd),
+          where("downloaded", "==", true),
+        );
+
+        // Fetch data for the current month
+        const currentMonthSnapshot = await getDocs(currentMonthQuery);
+        const currentMonthData = currentMonthSnapshot.docs.map((doc) =>
+          doc.data(),
+        );
+        setCurrentMonthDownloads(currentMonthData.length);
+
+        // Fetch data for the previous month
+        const previousMonthSnapshot = await getDocs(previousMonthQuery);
+        const previousMonthData = previousMonthSnapshot.docs.map((doc) =>
+          doc.data(),
+        );
+        setPreviousMonthDownloads(previousMonthData.length);
+
+        // Calculate percentage change
+        const percentageChangeValue =
+          currentMonthData.length !== 0
+            ? ((currentMonthData.length - previousMonthData.length) /
+                currentMonthData.length) *
+              100
+            : 0;
+        setPercentageChange(percentageChangeValue);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    console.log("useEffect completed");
+  }, []); // The empty dependency array ensures that this useEffect only runs once on component mount
+
+  useEffect(() => {
+    console.log("Setting currentMonthDownloads:", currentMonthDownloads);
+    console.log("Setting previousMonthDownloads:", previousMonthDownloads);
+    console.log("Setting percentageChange:", percentageChange);
+  }, [previousMonthDownloads, currentMonthDownloads, percentageChange]); // Add states as dependencies
+
   return (
     <>
       {page_loader && <Loader />}
@@ -64,6 +180,8 @@ export default function Home() {
               <Header position={"absolute"} padding={"0"} top={"0"} />
 
               <Dashboard_hero_section />
+
+              <div className="w-full sm:h-[15vw]"></div>
             </div>
           </div>
         </>
