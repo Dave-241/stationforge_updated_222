cae9a6c172b679a79db07afa2133e3d03054dbed
DAVIS ISIBOR <davisisibor@gmail.com>
2024-03-27 20:02:53 +0100

    Update fire_base_config.ts

--- src/app/utils/fire_base_config.ts
+++ src/app/utils/fire_base_config.ts
@@ -1,12 +1,12 @@
 // firebase-config.js or firebase-config.ts
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8tC6NhA3FehG0YE_nKPPSt4cYKm2YwkI",
-  authDomain: "fir-9-dojo-24129.firebaseapp.com",
-  projectId: "fir-9-dojo-24129",
-  storageBucket: "fir-9-dojo-24129.appspot.com",
-  messagingSenderId: "704117354230",
-  appId: "1:704117354230:web:eed710675958bfc820c6d7",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 export default firebaseConfig;
